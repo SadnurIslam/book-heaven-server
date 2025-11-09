5ca27644f1072e8be8ea797d944f4226acdff9ee
sadnur Islam <sadnurislam2000@gmail.com>
2025-11-09 14:07:45 +0600

    connected to mongodb

--- index.js
+++ index.js
@@ -1,11 +1,58 @@
 const express = require('express')
+require('dotenv').config()
 const app = express()
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = 3000
+
+app.use(express.json());
+app.use(cors());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.leame9e.mongodb.net/?appName=Cluster0`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const database = client.db("booksDB");
+    const booksCollection = database.collection("books");
+
+    app.post('/books', async (req, res) => {
+        const book = req.body;
+        console.log(book);
+        const result = await booksCollection.insertOne(book);
+
+        console.log(`A document was inserted with the _id: ${result.insertedId}`);
+        res.send(result);
+    })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`)
+  })
